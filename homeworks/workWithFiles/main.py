17580a5b101877ecd5946d926c84854134f04681
OleksandrKotyuk1703 <oleksandrk1703@gmail.com>
2022-01-17 17:55:04 +0200

    HW#8 "Work with files"

--- homeworks/workWithFiles/main.py
+++ homeworks/workWithFiles/main.py
@@ -1,9 +1,11 @@
 while True:
     num1 = int(input(" Enter the first number: "))
-    operator = input(" Enter operator:")
+    operator = input(" Enter operator: ")
     num2 = int(input(" Enter the second number: "))
     total = num1 + num2
     deduct = num1 - num2
+    multiple = num1 * num2
+    division = num1 / num2
 
     if operator == "+":
         print(total)
@@ -13,5 +15,14 @@ while True:
         print(deduct)
         with open('result.txt', 'a') as file:
             file.write(f"\n{num1} {operator} {num2} = {deduct}")
+    elif operator == "*":
+        print(multiple)
+        with open('result.txt', 'a') as file:
+            file.write(str(f"\n {num1} {operator} {num2} = {multiple} "))
+
+    elif operator == "/":
+        print(int(division))
+        with open('result.txt', 'a') as file:
+            file.write(str(f"\n {num1} {operator} {num2} = {division} "))
     else:
         print("Invalid operator")
