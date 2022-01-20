50b7a47cfa319f9d699e7a803551607373ce1959
OleksandrKotyuk1703 <oleksandrk1703@gmail.com>
2022-01-20 17:17:35 +0200

    HW#8 "Work With Files" fixed.

--- homeworks/workWithFiles/main.py
+++ homeworks/workWithFiles/main.py
@@ -1,28 +1,7 @@
-while True:
-    num1 = int(input(" Enter the first number: "))
-    operator = input(" Enter operator: ")
-    num2 = int(input(" Enter the second number: "))
-    total = num1 + num2
-    deduct = num1 - num2
-    multiple = num1 * num2
-    division = num1 / num2
+num1 = input("Enter the first number: ")
+operator = input("Enter operator: ")
+num2 = input("Enter the second number: ")
+calc = eval(num1 + operator + num2)
 
-    if operator == "+":
-        print(total)
-        with open('result.txt', 'a') as file:
-            file.write(f"\n{num1} {operator} {num2} = {total}")
-    elif operator == "-":
-        print(deduct)
-        with open('result.txt', 'a') as file:
-            file.write(f"\n{num1} {operator} {num2} = {deduct}")
-    elif operator == "*":
-        print(multiple)
-        with open('result.txt', 'a') as file:
-            file.write(str(f"\n {num1} {operator} {num2} = {multiple} "))
-
-    elif operator == "/":
-        print(int(division))
-        with open('result.txt', 'a') as file:
-            file.write(str(f"\n {num1} {operator} {num2} = {division} "))
-    else:
-        print("Invalid operator")
+with open('result.txt', 'a') as file:
+    file.write(f"{num1} {operator} {num2} = {calc}\n")
